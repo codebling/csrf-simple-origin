54760fa9ca3d493b9ca2a3aeb1285dbec25070f7
Code Bling <codeblingx@gmail.com>
2020-04-01 18:08:42 -0400

    Add tests

---
+++ test/index.js
@@ -0,0 +1,29 @@
+var originCompare = require('../index');
+
+var httpMocks = require('node-mocks-http');
+var chai = require('chai');
+var should = chai.should();
+
+
+it('should not accept domains that partially match', function(done) {
+  var allowedOrigins = ['localhost'];
+
+  var req = {
+    headers: {
+      'origin': 'localhost.example.com'
+    }
+  };
+  var res = httpMocks.createResponse({eventEmitter: require('events').EventEmitter});
+
+function assertFailure(err) {
+  chai.assert(err != null);
+  chai.assert(res.statusCode == 400);
+  done();
+}
+
+
+  res.on('end', function() {
+    
+  });
+  originCompare(allowedOrigins)(req, res, assertFailure);
+}) ;
