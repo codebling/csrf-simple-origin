51cc88557fd3d97098d32e39c38efb4fa3c27430
Code Bling <codeblingx@gmail.com>
2020-04-02 11:42:00 -0400

    Const improve test setup

--- test/index.js
+++ test/index.js
@@ -4,25 +4,33 @@ var httpMocks = require('node-mocks-http');
 var chai = require('chai');
 var should = chai.should();
 
+const createResponse = () => 
+  httpMocks.createResponse({eventEmitter: require('events').EventEmitter});
 
-it('should not accept domains that partially match', function(done) {
-  var allowedOrigins = ['localhost'];
+const assertError = done => err => {
+  chai.assert(err != null);
+  chai.assert(res.statusCode == 400);
+  done();
+};
 
-  var req = {
-    headers: {
-      'origin': 'localhost.example.com'
-    }
-  };
-  var res = httpMocks.createResponse({eventEmitter: require('events').EventEmitter});
 
-  function assertError(err) {
-    chai.assert(err != null);
-    chai.assert(res.statusCode == 400);
-    done();
-  }
+describe('Blocking', () => {
+  describe('Partial match', () => {
+    
+    it('should reply 400 when no options are specified for domains that partially match', done => {
+      const allowedOrigins = ['localhost'];
 
-  res.on('end', function() {
-    done(new Error('should not return'));
+      const req = {
+        headers: {
+          'origin': 'localhost.example.com'
+        }
+      };
+
+      const res = createResponse();
+      res.on('end', function() {
+        done(new Error('should not return'));
+      });
+      originCompare(allowedOrigins, )(req, res, () => );
+    });
   });
-  originCompare(allowedOrigins)(req, res, assertError);
-}) ;
+});
