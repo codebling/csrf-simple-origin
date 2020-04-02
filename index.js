689b018ed1cfbd79ba112f4052c4ea9e7ed68271
Code Bling <codeblingx@gmail.com>
2020-04-02 13:29:53 -0400

    Don't ever fall back to 'referer' header

--- index.js
+++ index.js
@@ -11,16 +11,10 @@ const normalizeOpts = {
 };
 
 const defaultOpts = {
-  compareFullPath: false,
   statusCode: 400,
   statusMessage: 'Deceptive Routing',
   failureHandler: false
 };
-
-function stripPath(allowedURL) {
-  const parsed = url.parse(allowedURL);
-  return parsed.protocol + ':' + (parsed.slashes ? '//' : '') + parsed.host;
-}
 
 module.exports = function(allowedOrigins, opts) {
   opts = Object.assign({}, defaultOpts, opts);
@@ -28,26 +22,13 @@ module.exports = function(allowedOrigins, opts) {
   allowedOrigins = allowedOrigins.map(function(allowedOrigin) {
     return normalizeUrl(allowedOrigin, normalizeOpts);
   });
-  if(!opts.compareFullPath) {
-    allowedOrigins = allowedOrigins.map(stripPath);
-  }
 
   return function(req, res, next) {
     const origin = req.headers['origin'];
-    const referer = req.headers['referer'];
 
-    //set "source origin" from the Origin header,
-    // falling back to referer if that header is not preset.
-    // Older browsers do not support it yet, and it seems like
-    // even in supported browsers there are some cases where it isn't sent
-    let sourceOrigin = origin || referer || '';
+    const normalizedOrigin = normalizeUrl(origin, normalizeOpts);
 
-    sourceOrigin = normalizeUrl(sourceOrigin, normalizeOpts);
-    if(!opts.compareFullPath) {
-      sourceOrigin = stripPath(sourceOrigin);
-    }
-
-    if(allowedOrigins.indexOf(sourceOrigin) != -1) {
+    if(allowedOrigins.indexOf(normalizedOrigin) != -1) {
       next();
     } else {
       if(opts.failureHandler) {
